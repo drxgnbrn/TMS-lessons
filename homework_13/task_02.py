14f9dcbd04f68ab8176284f2d8ab755ba695b3ec
drxgnbrn <123400669+drxgnbrn@users.noreply.github.com>
2023-03-12 21:28:53 +0300

    Delete task_02.py

--- homework_13/task_02.py
+++
@@ -1,9 +0,0 @@
-import re
-def is_date(string: str):
-    return re.fullmatch(r'\d{1,2}-\d{1,2}-\d{4}', string) is not None
-
-if __name__ == '__main__':
-    assert is_date('23-06-2004')
-    assert is_date('23-14-2000')
-    # assert not is_date('23-06-2004')
-    assert not is_date('-06-2004')
