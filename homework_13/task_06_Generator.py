0f654b9f04cb9a56fff301c933d05e7ce2edb45a
Egor <drxgnbrn@gmail.com>
2023-03-12 21:26:00 +0300

    Add homework

---
+++ homework_13/task_06_Generator.py
@@ -0,0 +1,12 @@
+def generate_words(text):
+    for word in text.split():
+        yield word
+
+text = "Vsem privet rebyata kak vashi dela"
+for word in generate_words(text):
+    print(word)
+
+if __name__ == '__main__':
+    assert generate_words('poka rebyta ahaha')
+    assert generate_words('poka rebyta , ahaha')
+    assert not generate_words('poka rebyta ahaha')
---
+++ homework_13/task_test_iter.py
@@ -0,0 +1,19 @@
+class LetterIterable:
+    def __init__(self, letter: str):
+        self.letters = letter
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index < len(self.letters):
+            letter = self.letters[self.index]
+            self.index += 1
+            return letter
+        raise StopIteration()
+
+letter = "Hello world! This is a test string."
+letters = LetterIterable(letter)
+for letter in letters:
+    print(letter)
